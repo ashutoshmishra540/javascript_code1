38efde93ed5ddb40abe2c70ca6235bd982adb49e
ashutoshmishra540 <124282090+ashutoshmishra540@users.noreply.github.com>
2024-02-11 06:19:59 +0000

    datatype

---
+++ 01_basic.js
@@ -0,0 +1,5 @@
+let name = "ashutosh";
+const number = 9958779812;
+var emaiId = "mishraashutosh@gmail.com";
+
+console.table([name, number, emailId]);
---
+++ 02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code new version.
+
+let name = "ashutosh"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+// number => 2 tp power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null = standaloan value 
+// undefined
+// symbol => unique
+
+// object
+
+console.table([name, age, isLoggedIn,]);
+
+console.log(typeof age); // object
